cdd1b3c5641de97e5811d0aeddf24890fe79e44d
Alexis <alegw1@gmail.com>
2023-01-31 12:46:03 -0300

    Subida del proyecto al repositorio

---
+++ views/partials/script.js
@@ -0,0 +1,10 @@
+{/* <script>
+    const fullImgBox = document.getElementById('fullImgBox');
+    const fullImg = document.getElementById("fullImg");
+
+    function closeImg(){
+        fullImgBox.style.display = "none"
+    }
+</script> */}
+
+/** NO SE PUEDE VINCULAR CORRECTAMENTE, POR ESO LO INCORPORÈ DENTRO DE LA VISTA INDEX */
